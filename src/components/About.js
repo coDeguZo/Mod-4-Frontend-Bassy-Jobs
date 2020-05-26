709aabdaefd6fa0e7b45c46f6a7a80453bae50b9
kaidornel <kai_dornel@hotmail.com>
2020-05-25 22:59:28 -0400

    styled about page

--- src/components/About.js
+++ src/components/About.js
@@ -1,16 +1,80 @@
 import React from "react"
+// import twitter from '../images/twitter.png'
 
 const About = (props) => {
+    // let twitter = require(`../images/twitter.png`)
     return (
     // <div className="myDiv">
     <div className="about-page">
-        {/* <div className="bg"></div> */}
-        <div className="about-page-text-container"></div>
-        Hi there
-        <p className="about-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Felis eget nunc lobortis mattis aliquam faucibus. Turpis egestas maecenas pharetra convallis. Hendrerit gravida rutrum quisque non tellus. Bibendum enim facilisis gravida neque convallis a. Hac habitasse platea dictumst quisque sagittis purus. Ut placerat orci nulla pellentesque dignissim enim. Viverra accumsan in nisl nisi scelerisque eu ultrices. Tempor orci dapibus ultrices in iaculis nunc. Eget magna fermentum iaculis eu non. Maecenas volutpat blandit aliquam etiam erat velit scelerisque in. Pretium quam vulputate dignissim suspendisse in est ante in. Ut morbi tincidunt augue interdum velit euismod in pellentesque. Interdum varius sit amet mattis vulputate enim nulla aliquet porttitor. Enim ut sem viverra aliquet eget sit amet tellus. Dignissim convallis aenean et tortor at. Urna nec tincidunt praesent semper feugiat nibh. Integer quis auctor elit sed vulputate mi sit amet mauris. Diam maecenas sed enim ut sem viverra aliquet. Et netus et malesuada fames.</p>
-        <p className="about-text">Massa massa ultricies mi quis hendrerit dolor magna. Tristique nulla aliquet enim tortor at auctor. Non consectetur a erat nam at lectus. Sed blandit libero volutpat sed. Erat pellentesque adipiscing commodo elit at imperdiet dui accumsan sit. Massa vitae tortor condimentum lacinia quis vel eros donec. Proin nibh nisl condimentum id venenatis. A condimentum vitae sapien pellentesque habitant morbi tristique senectus et. Habitasse platea dictumst quisque sagittis purus sit amet volutpat. Orci nulla pellentesque dignissim enim sit amet venenatis urna cursus. Sodales neque sodales ut etiam sit amet nisl. Sodales neque sodales ut etiam sit. Ornare arcu dui vivamus arcu felis bibendum ut. Eu augue ut lectus arcu bibendum at varius vel pharetra. In cursus turpis massa tincidunt dui ut ornare lectus sit.</p>
+        <div className="about-text">
+            <br/>
+        <h3>Our Mission</h3>
+            <p>Lorem Ipsum dolor sit amet, consectetur adipiscing elit</p>
+        <h3>Our Values</h3>
+            <p>Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua</p>
+            <p>Felis eget nunc lobortis mattis aliquam faucibus</p>
+            <p>Turpis egestas maecenas pharetra convallis</p>
+        <h3>About Us</h3>
+        <div className="about-us-text-container"></div>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Felis eget nunc lobortis mattis aliquam faucibus. Turpis egestas maecenas pharetra convallis. Hendrerit gravida rutrum quisque non tellus. Bibendum enim facilisis gravida neque convallis a. Hac habitasse platea dictumst quisque sagittis purus. Ut placerat orci nulla pellentesque dignissim enim. Viverra accumsan in nisl nisi scelerisque eu ultrices. Tempor orci dapibus ultrices in iaculis nunc. Eget magna fermentum iaculis eu non. Maecenas volutpat blandit aliquam etiam erat velit scelerisque in. Pretium quam vulputate dignissim suspendisse in est ante in. Ut morbi tincidunt augue interdum velit euismod in pellentesque. Interdum varius sit amet mattis vulputate enim nulla aliquet porttitor. Enim ut sem viverra aliquet eget sit amet tellus. Dignissim convallis aenean et tortor at. Urna nec tincidunt praesent semper feugiat nibh. Integer quis auctor elit sed vulputate mi sit amet mauris. Diam maecenas sed enim ut sem viverra aliquet. Et netus et malesuada fames.</p>
+        <p>Massa massa ultricies mi quis hendrerit dolor magna. Tristique nulla aliquet enim tortor at auctor. Non consectetur a erat nam at lectus. Sed blandit libero volutpat sed. Erat pellentesque adipiscing commodo elit at imperdiet dui accumsan sit. Massa vitae tortor condimentum lacinia quis vel eros donec. Proin nibh nisl condimentum id venenatis. A condimentum vitae sapien pellentesque habitant morbi tristique senectus et. Habitasse platea dictumst quisque sagittis purus sit amet volutpat. Orci nulla pellentesque dignissim enim sit amet venenatis urna cursus. Sodales neque sodales ut etiam sit amet nisl. Sodales neque sodales ut etiam sit. Ornare arcu dui vivamus arcu felis bibendum ut. Eu augue ut lectus arcu bibendum at varius vel pharetra. In cursus turpis massa tincidunt dui ut ornare lectus sit.</p>
+        <a href="https://google.com" id="about-blog">Check Out Our Blog Here</a>
+        <br></br>
+        <h3>Learn More and Follow Us Here</h3>
+        <div className="learn-parts">
+            <a href="https://twitter.com">
+                <img src="../images/twitter.png" alt="twitter" id="twitter-icon" className="about-icon"></img>
+            </a>
+            <a href="https://instagram.com">
+                <img src="../images/instagram.png" alt="instagram" id="instagram-icon" className="about-icon"></img>
+            </a>
+            <a href="https://facebook.com">
+                <img src="../images/facebook.png" alt="facebook" id="facebook-icon" className="about-icon"></img>
+            </a>
+            <a href="https://linkedin.com">
+                <img src="../images/linkedin.png" alt="linkedin" id="linkedin-icon" className="about-icon"></img>
+            </a>
+        </div>
+        </div>
     </div>
     )
 }
 
 export default About
+
+// import React from "react"
+// import twitter from `../images/twitter.png`
+
+// const Application = (props) => {
+
+//     // let twitter = require(`../images/twitter.png`)
+
+// const About = (props) => {
+//     return (
+//     // <div className="myDiv">
+//     <div className="about-page">
+//         <div className="about-text">
+//             <br/>
+//         <h3>Our Mission</h3>
+//             <p>Lorem Ipsum dolor sit amet, consectetur adipiscing elit</p>
+//         <h3>Our Values</h3>
+//             <p>Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua</p>
+//             <p>Felis eget nunc lobortis mattis aliquam faucibus</p>
+//             <p>Turpis egestas maecenas pharetra convallis</p>
+//         <h3>About Us</h3>
+//         <div className="about-us-text-container"></div>
+//         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Felis eget nunc lobortis mattis aliquam faucibus. Turpis egestas maecenas pharetra convallis. Hendrerit gravida rutrum quisque non tellus. Bibendum enim facilisis gravida neque convallis a. Hac habitasse platea dictumst quisque sagittis purus. Ut placerat orci nulla pellentesque dignissim enim. Viverra accumsan in nisl nisi scelerisque eu ultrices. Tempor orci dapibus ultrices in iaculis nunc. Eget magna fermentum iaculis eu non. Maecenas volutpat blandit aliquam etiam erat velit scelerisque in. Pretium quam vulputate dignissim suspendisse in est ante in. Ut morbi tincidunt augue interdum velit euismod in pellentesque. Interdum varius sit amet mattis vulputate enim nulla aliquet porttitor. Enim ut sem viverra aliquet eget sit amet tellus. Dignissim convallis aenean et tortor at. Urna nec tincidunt praesent semper feugiat nibh. Integer quis auctor elit sed vulputate mi sit amet mauris. Diam maecenas sed enim ut sem viverra aliquet. Et netus et malesuada fames.</p>
+//         <p>Massa massa ultricies mi quis hendrerit dolor magna. Tristique nulla aliquet enim tortor at auctor. Non consectetur a erat nam at lectus. Sed blandit libero volutpat sed. Erat pellentesque adipiscing commodo elit at imperdiet dui accumsan sit. Massa vitae tortor condimentum lacinia quis vel eros donec. Proin nibh nisl condimentum id venenatis. A condimentum vitae sapien pellentesque habitant morbi tristique senectus et. Habitasse platea dictumst quisque sagittis purus sit amet volutpat. Orci nulla pellentesque dignissim enim sit amet venenatis urna cursus. Sodales neque sodales ut etiam sit amet nisl. Sodales neque sodales ut etiam sit. Ornare arcu dui vivamus arcu felis bibendum ut. Eu augue ut lectus arcu bibendum at varius vel pharetra. In cursus turpis massa tincidunt dui ut ornare lectus sit.</p>
+//         <br></br>
+//         <h3>Learn More</h3>
+//         <div className="learn-parts">
+//             <a class="twitter socialIcon" href="https://twitter.com">
+//                 {/* <img src="" */}
+//             </a>
+//         </div>
+//         </div>
+//     </div>
+//     )
+// }
+
+// export default About;
